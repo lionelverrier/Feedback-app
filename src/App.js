8d9b5af1f9a0af898611978047c154d49a2f7ea0
Lionel Verrier <lgv3lee@gmail.com>
2023-03-07 17:20:52 -0500

    feedbackForm and shared button components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
 import Card from './components/shared/Card';
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
     <>
       <Header />
       <div className='container'>
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
