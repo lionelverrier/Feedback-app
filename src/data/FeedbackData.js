5fe2bb8e7f88e16f5d49d8ac966dc0dc351b0f06
Lionel Verrier <lgv3lee@gmail.com>
2023-03-07 16:09:19 -0500

    feedback items, list delete

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,14 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'liorem ipusm dolor sit amet consectetur adipisicing elit. consequuntur =vel vitate commodi alias voluptatatem est vluptatum ipsa quae.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipusm dolor sit amet consectetur adipiscing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quea',
+  },
+];
+
+export default FeedbackData
