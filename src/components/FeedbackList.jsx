7e580b3b041be8d90354043125bb909361ff12fb
Lionel Verrier <lgv3lee@gmail.com>
2023-03-10 20:41:13 -0500

    framer motion feedback list

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types';
 
@@ -8,15 +9,37 @@ function FeedbackList({ feedback, handleDelete }) {
   }
   return (
     <div className='feedback-list'>
-      {feedback.map((item) => (
-        <FeedbackItem
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
           key={item.id}
-          item={item}
-          handleDelete={handleDelete}
-        />
-      ))}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+          //Without list fading animation
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem
+  //         key={item.id}
+  //         item={item}
+  //         handleDelete={handleDelete}
+  //       />
+  //     ))}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {
